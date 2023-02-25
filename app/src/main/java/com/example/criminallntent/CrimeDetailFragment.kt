c0a9b60ab014818bf64244ff5d20de43ab6f1e7c
major <majormoku0510@gmail.com>
2023-02-25 15:56:10 -0500

    add some comments

--- app/src/main/java/com/example/criminallntent/CrimeDetailFragment.kt
+++ app/src/main/java/com/example/criminallntent/CrimeDetailFragment.kt
@@ -12,13 +12,13 @@ import java.util.*
 class CrimeDetailFragment : Fragment() {
     private lateinit var crime: Crime
 //    private lateinit var binding: FragmentCrimeDetailBinding
-    private var _binding: FragmentCrimeDetailBinding? = null
+    private var _binding: FragmentCrimeDetailBinding? = null // 为了deastroy的正常使用
     private val binding
         get() = checkNotNull(_binding){
             "can not access binding cause it is null. Is the view visible?"
         }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {  //初始化
         super.onCreate(savedInstanceState)
         crime =Crime(
             id = UUID.randomUUID(),
@@ -28,17 +28,17 @@ class CrimeDetailFragment : Fragment() {
         )
     }
 
-    override fun onCreateView(
+    override fun onCreateView(  // inflate and bind the layout for the fragment and return the inflated view
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 //        binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
-        _binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false)
-        return binding.root
+        _binding = FragmentCrimeDetailBinding.inflate(layoutInflater, container, false) // inflate它的作用是将一个layout.xml布局文件变为一个View对象,加载一个布局
+        return binding.root //binding.root表示最外层的View
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {  // wire up
         super.onViewCreated(view, savedInstanceState)
         binding.apply{
             crimeTitle.doOnTextChanged { text, _, _, _ -> // set a listener
--- app/src/main/java/com/example/criminallntent/CrimeListAdapter.kt
+++ app/src/main/java/com/example/criminallntent/CrimeListAdapter.kt
@@ -22,13 +22,13 @@ class CrimeHolder(private val binding: ListItemCrimeBinding) : RecyclerView.View
 }
 
 class CrimeListAdapter(private val crimes: List<Crime>) : RecyclerView.Adapter<CrimeHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeHolder { // 负责承载每个子项的布局。
         val inflater = LayoutInflater.from(parent.context)
         val binding = ListItemCrimeBinding.inflate(inflater, parent,false)
         return CrimeHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+    override fun onBindViewHolder(holder: CrimeHolder, position: Int) { // 负责将每个子项holder绑定数据。
         val crime = crimes[position]
 //        holder.apply {
 //            binding.crimeTitle.text = crime.title
