b4a70a0466391dd7c8c4cd8941eaed0a50caaa46
major <majormoku0510@gmail.com>
2023-03-02 15:11:04 -0500

    add some test

--- app/src/test/java/com/example/criminallntent/ExampleUnitTest.kt
+++ app/src/test/java/com/example/criminallntent/ExampleUnitTest.kt
@@ -10,8 +10,19 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+    val a=CrimeListViewModel()
     @Test
+
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+    @Test
+    fun test_list(){
+        assertEquals(100,a.crimes.size)
+    }
+    @Test
+    fun test_type(){
+        val adp=CrimeListAdapter(a.crimes)
+        assertEquals(1,adp.getItemViewType(1))
     }
 }
